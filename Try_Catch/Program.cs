02cfbdb528bb725ebc4234584cd60f69534d6f07
João Vitor <jvitoralvernaz@outlook.com>
2023-06-05 17:25:04 -0400

    finally

--- Try_Catch/Program.cs
+++ Try_Catch/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 
-try
+while (true)
 {
-    Console.Write("Digite o primeiro número: ");
-    double a = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Digite o segundo número: ");
-    double b = Convert.ToDouble(Console.ReadLine());
+    try
+    {
+        Console.Write("Digite o primeiro número: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o segundo número: ");
+        double b = Convert.ToDouble(Console.ReadLine());
 
-    double c = a / b;
+        double c = a / b;
 
-    Console.WriteLine($"Seu resultado é : {c}");
-}
-catch(DivideByZeroException ex)
-{
-    Console.WriteLine("Não permitido realizar divisão por zero!");
-}
-catch(FormatException ex)
-{
-    Console.WriteLine("Não permitido realizar divizão com letras!");
-}
-catch(Exception ex)
-{
-    Console.WriteLine($"Ocorreu um erro {ex.Message}");
+        Console.WriteLine($"Seu resultado é : {c}");
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("Não permitido realizar divisão por zero!");
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Não permitido realizar divizão com letras!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um erro {ex.Message}");
+    }
+    finally
+    {
+        Console.WriteLine("Aqui passa de qualquer forma!");
+    }
 }
