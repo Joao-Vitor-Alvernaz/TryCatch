30aeb17eef335670b1488cc2396d155655d48dd5
João Vitor <jvitoralvernaz@outlook.com>
2023-06-05 17:09:45 -0400

    Correções dos esrros especificos

--- Try_Catch/Program.cs
+++ Try_Catch/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+try
+{
+    Console.Write("Digite o primeiro número: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Digite o segundo número: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+
+    double c = a / b;
+
+    Console.WriteLine($"Seu resultado é : {c}");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine("Não permitido realizar divisão por zero!");
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Não permitido realizar divizão com letras!");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro {ex.Message}");
+}
